bf7ed92cb3e3bb10dd758b47b843aed053f42e88
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2022-12-15 00:09:40 +0100

    Monty

--- set_op_toks_error.c
+++ set_op_toks_error.c
@@ -6,31 +6,31 @@
  */
 void set_op_tok_error(int error_code)
 {
-	int toks_len = 0, i = 0;
+	int token_length = 0, i = 0;
 	char *exit_str = NULL;
-	char **new_toks = NULL;
+	char **new_token = NULL;
 
-	toks_len = token_arr_len();
-	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+	token_length = token_arr_len();
+	new_token = malloc(sizeof(char *) * (token_length + 2));
 	if (!op_toks)
 	{
 		malloc_error();
 		return;
 	}
-	while (i < toks_len)
+	while (i < token_length)
 	{
-		new_toks[i] = op_toks[i];
+		new_token[i] = op_toks[i];
 		i++;
 	}
 	exit_str = get_int(error_code);
 	if (!exit_str)
 	{
-		free(new_toks);
+		free(new_token);
 		malloc_error();
 		return;
 	}
-	new_toks[i++] = exit_str;
-	new_toks[i] = NULL;
+	new_token[i++] = exit_str;
+	new_token[i] = NULL;
 	free(op_toks);
-	op_toks = new_toks;
+	op_toks = new_token;
 }
